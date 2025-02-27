603282f8d1970942ace5dce9c80727a18252992f
roconnell15 <108817083+roconnell15@users.noreply.github.com>
2025-02-27 14:03:16 +0000

    Pushing image filter

---
+++ projects/asd-image-filtering-main/index.js
@@ -0,0 +1,61 @@
+// This is a small program. There are only two sections. This first section is what runs
+// as soon as the page loads.
+$(document).ready(function () {
+  render($("#display"), image);
+  $("#apply").on("click", applyAndRender);
+  $("#reset").on("click", resetAndRender);
+});
+
+/////////////////////////////////////////////////////////
+//////// event handler functions are below here /////////
+/////////////////////////////////////////////////////////
+
+// this function resets the image to its original value; do not change this function
+function resetAndRender() {
+  reset();
+  render($("#display"), image);
+}
+
+// this function applies the filters to the image and is where you should call
+// all of your apply functions
+function applyAndRender() {
+  // Multiple TODOs: Call your apply function(s) here
+applyFilter()
+  
+
+  // do not change the below line of code
+  render($("#display"), image);
+}
+
+/////////////////////////////////////////////////////////
+// "apply" and "filter" functions should go below here //
+/////////////////////////////////////////////////////////
+
+// TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(){
+  for(var x = 0; x < image.length; x++){
+    var row = image[x]
+    for(var i = 0; i < row.length; i++){
+      var rgbString = row[i]
+      var rgbNumbers = rgbStringToArray(rgbString)
+      rgbNumbers[RED] = 255
+      rgbNumbers[GREEN] = 0
+      rgbString = rgbStringToArray(rgbNumbers)
+      row[i] = rgbString
+    }
+  }
+}
+
+// TODO 7: Create the applyFilterNoBackground function
+
+
+// TODO 5: Create the keepInBounds function
+
+
+// TODO 3: Create reddify function
+
+
+// TODO 6: Create more filter functions
+
+
+// CHALLENGE code goes below here
